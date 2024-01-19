cd55cb73128e951013c86c1cc3a976115f1541f7
Lucia Harcegova <fakeluciamail@gmail.com>
2024-01-19 14:30:29 +0100

    Change deploy server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '188.166.108.74', user: 'deploy', roles: %w{app db web}
+server '188.166.108.67', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
