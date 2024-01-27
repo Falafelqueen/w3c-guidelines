9e4ebd5864de264b2729fd5d47db3c7b7da8edc8
Lucia Harcegova <fakeluciamail@gmail.com>
2024-01-27 09:11:29 +0100

    Optimize for phone

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -7,34 +7,47 @@ export default class extends Controller {
 
   toggleOpen(event) {
     event.preventDefault();
+
     const ariaValue = event.target.getAttribute("aria-expanded") == "true";
     const cardContainer = document.querySelector(".card__container");
+    const appliedFilters = document.querySelector(".applied-filters");
+
+    const screenSize = window.innerWidth > 0 ? window.innerWidth : screen.width;
+
+    console.log("screenSize", screenSize);
 
     event.target.setAttribute("aria-expanded", !ariaValue);
-    /* toggle class */
-    console.log(
-      "before: showing classlist from toggleOpen",
-      this.listTarget.classList
-    );
+
     this.listTarget.classList.toggle("display__list-container--active");
 
-    const appliedFilters = document.querySelector(".applied-filters");
-    if (this.listTarget.classList.contains("display__list-container--active")) {
-      const extraMargin = this.listTarget.offsetHeight;
-      if (appliedFilters) {
-        appliedFilters.style.marginTop = `${extraMargin}px`;
+    /* On mobile devices */
+    if (screenSize < 415) {
+      if (
+        this.listTarget.classList.contains("display__list-container--active")
+      ) {
+        /* Move everything down after opening the popup  */
+        const extraMargin = this.listTarget.offsetHeight;
+        if (appliedFilters) {
+          appliedFilters.style.marginTop = `${extraMargin}px`;
+        } else {
+          cardContainer.style.marginTop = `${extraMargin}px`;
+        }
+
+        /* Close other opened dropdowns */
+        const dropdowns = document.querySelectorAll(
+          ".display__list-container--active"
+        );
+        dropdowns.forEach((dropdown) => {
+          if (dropdown != this.listTarget) {
+            dropdown.classList.remove("display__list-container--active");
+          }
+        });
       } else {
-        cardContainer.style.marginTop = `${extraMargin}px`;
+        if (appliedFilters) {
+          appliedFilters.style.marginTop = "initial";
+        }
+        cardContainer.style.marginTop = "initial";
       }
-    } else {
-      if (appliedFilters) {
-        appliedFilters.style.marginTop = "initial";
-      }
-      cardContainer.style.marginTop = "initial";
     }
-    console.log(
-      "after: showing classlist from toggleOpen",
-      this.listTarget.classList
-    );
   }
 }
